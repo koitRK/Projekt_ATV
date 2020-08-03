82c7540e74c371fb837fa7085469d82a26b6edc7
AlanRaak <56818442+AlanRaak@users.noreply.github.com>
2020-08-03 14:28:05 +0300

    Update Try_include.ino
    lol

--- Arduino_files/Try_include/Try_include.ino
+++ Arduino_files/Try_include/Try_include.ino
@@ -9,7 +9,8 @@ int magnet_sensor = A0;
 int motor_step = 6;
 int motor_direction = 7;
 int motor_enable = 8;
-int linear_enable = 9;
+int linear_enable = 4;
+// pin 9 on vist atv mootori suund, ehk tagukäik
 int linear_direction = 13;
 int throttle_pin = 5;
 int encoder_CLK_pin = 10; //Digital Pin 10
@@ -20,9 +21,9 @@ int encoder_CSn_pin = 12; //Digital Pin 12
 
 unsigned int encoder_reading;
 int encoder_value;
-int encoder_middle_value = 175;  //100 - 250
-int encoder_left_value = 250;
-int encoder_right_value = 100;
+int encoder_middle_value = 221;  //100 - 250 seda ei kasuta nikuinii
+int encoder_left_value = 302;
+int encoder_right_value = 130;
 int target_pos = 2500;
 int current_pos = 2500;
 unsigned long int a,b,c;
@@ -31,8 +32,8 @@ bool dir = false;
 bool enabled = true;
 int counter = 0;
 int magnet_value; // analog readings
-int brake_off_value = 440;
-int brake_on_value = 520;
+int brake_off_value = 440; //440 original
+int brake_on_value = 520;  //520 original
 int target_brake_value;
 int current_brake_value;
 bool pause_steering = false;
@@ -78,8 +79,8 @@ void loop() {
   magnet_value = analogRead(magnet_sensor);
 
 
-  /*
-  // START - BRAKE
+ 
+  // START - BRAKE(and throttle)
   //Serial.println(magnet_value);
   
   //new
@@ -87,37 +88,38 @@ void loop() {
     analogWrite(throttle_pin, 40);  // To stop motor from running
     input_brake_percent = ch[3] / 490.0; // 1 -> 0, 1 is off, 0 is fully on
     output_brake_percent = (brake_on_value - magnet_value) / float(brake_on_value - brake_off_value); // over 1 is over off, 1 is off, 0 is full on,
-    //Serial.println(magnet_value);
-    if (input_brake_percent < output_brake_percent){
+    
+    if (input_brake_percent < output_brake_percent){ // Should apply brake
       digitalWrite(linear_enable, HIGH);
       digitalWrite(linear_direction, LOW);
+      Serial.println(777);
       }
-    else if (input_brake_percent > output_brake_percent){
+    else if (input_brake_percent > output_brake_percent){  // Should disengage brake
       digitalWrite(linear_enable, HIGH);
       digitalWrite(linear_direction, HIGH);
+      Serial.println("££###££");
       }
     }
-  else if (magnet_value > brake_off_value){
-    analogWrite(throttle_pin, 40);  // To stop motor from running
+  else if (magnet_value > brake_off_value){  // Takes off brake and stops motor
+    analogWrite(throttle_pin, 40);
     digitalWrite(linear_enable, HIGH);
     digitalWrite(linear_direction, HIGH);
     }
   else{
-    digitalWrite(9, LOW); // To stop brake from going over off position
-    if (990 > ch[3] && ch[3] > 550){
+    digitalWrite(linear_enable, LOW); // To stop brake from going over off position
+    if (990 > ch[3] && ch[3] > 550){  // Starts throttle
       throttle = map(ch[3], 520, 980, 40, 240);
-      analogWrite(throttle_pin, throttle);
+      analogWrite(throttle_pin, throttle);  // Throttles motor
       }
     else{
       //IDLE - Handbrake maybe
       }
     }
 
-  */
-    
   // END - BRAKE
-  // START - STEERING
 
+  
+  // START - STEERING
   
   target_pos = map(ch[1], 0, 1000, encoder_left_value, encoder_right_value);
 
@@ -140,7 +142,9 @@ void loop() {
   counter += 1;
   
   if (counter >= 50){
-    Serial.print(ch[1]);
+    Serial.println(encoder_value);
+    Serial.print("------");
+    Serial.print(ch[3]);
     Serial.print("    ");
     Serial.print(target_pos);
     Serial.print("   ");
