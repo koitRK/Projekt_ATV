d46ecaefb5e792a672eacb1adbf3d0adba322a9d
AlanRaak <56818442+AlanRaak@users.noreply.github.com>
2020-09-10 14:43:14 +0300

    Delete RC_reader.ino

--- Arduino_files/Main/RC_reader.ino
+++
@@ -1,9 +0,0 @@
-void ReadRCValues(){  // Reads RC channels
-  
-  int RC_count = 0;
-  
-  while (ppmReader.get(RC_count) != 0) {
-    RC_channel_values[RC_count] = ppmReader.get(RC_count);
-    RC_count++;
-  }
-}
